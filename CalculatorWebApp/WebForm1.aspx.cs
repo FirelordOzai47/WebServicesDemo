2dd55399cb0c836dbb54e45cfefab4f24d9fcf4c
FirelordOzai47 <mustafadgn.4@gmail.com>
2023-03-04 14:33:46 +0300

    Add project files.

---
+++ CalculatorWebApp/WebForm1.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CalculatorWebApp
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            CalculatorService.CalculatorWebServiceSoapClient clint = new CalculatorService.CalculatorWebServiceSoapClient();
+            int result = clint.Add(Convert.ToInt32(txtFirstNumber.Text), (Convert.ToInt32(txtSecondNumber.Text)));
+            lblResult.Text= result.ToString();
+        }
+    }
+}
---
+++ WebServicesDemo/CalculatorWebService.asmx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace WebServicesDemo
+{
+    /// <summary>
+    /// Summary description for CalculatorWebService
+    /// </summary>
+    [WebService(Namespace = "http://mustafadogancv.com.tr/webservices")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class CalculatorWebService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public int Add(int number1,int number2)
+        {
+            return number1+number2;
+        }
+    }
+}
